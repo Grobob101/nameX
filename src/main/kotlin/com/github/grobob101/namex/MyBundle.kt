fbbbdd28af8b89747cd0511c3fe486ae53a649a3
GitHub Action <action@github.com>
2024-08-20 14:16:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/grobob101/namex/MyBundle.kt
+++ src/main/kotlin/com/github/grobob101/namex/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.grobob101.namex
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/grobob101/namex/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/grobob101/namex/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.grobob101.namex.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
